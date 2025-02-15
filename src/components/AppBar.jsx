a06d63fa385a9c0fa7538ce99f24c15c7d92b4d7
Teemu Himanen <thimanen@gmail.com>
2025-02-15 21:58:07 +0200

    10.7: scrollable app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import { Link } from "react-router-native";
 import Constants from "expo-constants";
 import Text from "./Text";
@@ -17,13 +17,22 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5,
   },
+  scrollView: {
+    horizontal: "true",
+  },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab title="Repositories" toLink="/" style={{ marginLeft: 25 }} />
-      <AppBarTab title="Sign in" toLink="/signin" style={{ marginLeft: 25 }} />
+      <ScrollView horizontal={true}>
+        <AppBarTab title="Repositories" toLink="/" style={{ marginLeft: 25 }} />
+        <AppBarTab
+          title="Sign in"
+          toLink="/signin"
+          style={{ marginLeft: 25 }}
+        />
+      </ScrollView>
     </View>
   );
 };
